ece40efd5853895c093ced7270c89dca5f88a8c3
Jim Daniels Wasswa <jim@deriv.com>
2023-06-05 10:50:27 +0800

    ci: add third screenshot

--- end-to-end-test/utils/helpers.ts
+++ end-to-end-test/utils/helpers.ts
@@ -7,12 +7,13 @@ import { TBrowserName, TPerformanceResults } from './types';
 export const switchAccountType = async (page: Page, from_type: string, to_type: string) => {
     await page.screenshot({ path: 'screenshots/switchAccountType1.png' });
     await page.waitForLoadState('domcontentloaded');
+    await page.screenshot({ path: 'screenshots/switchAccountType2.png' });
     await page.waitForSelector('.account-type-dropdown--parent', { timeout: 0 });
     await page.waitForSelector(`.account-type-dropdown--${from_type.toLowerCase()}`, { timeout: 0 });
     await page.getByTestId('dti_dropdown_display').getByText(from_type).click({ timeout: 0 });
     await page.locator(`#${to_type.toLocaleLowerCase()}`).click();
     await page.waitForLoadState('domcontentloaded');
-    await page.screenshot({ path: 'screenshots/switchAccountType2.png' });
+    await page.screenshot({ path: 'screenshots/switchAccountType3.png' });
 };
 export const enablePerformance = async (page: Page, browserName) => {
     let client: CDPSession | null = null;
