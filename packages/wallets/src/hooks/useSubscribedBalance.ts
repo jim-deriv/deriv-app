bc0d9198950553ec5cc9a39c15bf008ffc81f9a7
Jim Daniels Wasswa <jim@regentmarkets.com>
2024-06-27 18:33:12 +0800

    refactor: extract class to its own file

--- packages/wallets/src/hooks/useSubscribedBalance.ts
+++ packages/wallets/src/hooks/useSubscribedBalance.ts
@@ -1,43 +1,8 @@
 import { useEffect, useState } from 'react';
 import { useBalanceSubscription } from '@deriv/api-v2';
+import Observable from '../utils/observable';
 
 type TBalance = ReturnType<typeof useBalanceSubscription>['data'];
-
-type Subscriber<T> = (value: T) => void;
-
-class Observable<T> {
-    private subscribers = new Set<Subscriber<T>>();
-
-    constructor(private value: T) {
-        this.value = value;
-    }
-
-    get(): T {
-        return this.value;
-    }
-
-    set(newValue: T): void {
-        if (JSON.stringify(this.value) === JSON.stringify(newValue)) return; // prevent unnecessary updates, causing infinite re-renders
-        this.value = newValue;
-
-        this.subscribers.forEach(listener => listener(this.value)); // notify all subscribers
-    }
-
-    /**
-     * @description Subscribes to the observable
-     * @param subscriber the observer function
-     * @returns cleanup function to unsubscribe the subscriber when the component unmounts.
-     */
-    subscribe(subscriber: Subscriber<T>): () => void {
-        this.subscribers.add(subscriber);
-
-        return () => this.unsubscribe(subscriber); // cleanup function to unsubscribe the subscriber when the component unmounts
-    }
-
-    unsubscribe(subscriber: Subscriber<T>): void {
-        this.subscribers.delete(subscriber);
-    }
-}
 
 const balanceStore = new Observable<TBalance | undefined>(undefined);
 
