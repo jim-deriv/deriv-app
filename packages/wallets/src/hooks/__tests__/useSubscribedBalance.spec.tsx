d272ee1ab3619b0c6ae7a82d06b27db4e3837ee1
Jim Daniels Wasswa <jim@regentmarkets.com>
2024-07-01 14:46:01 +0800

    test: add more tests

--- packages/wallets/src/hooks/__tests__/useSubscribedBalance.spec.tsx
+++ packages/wallets/src/hooks/__tests__/useSubscribedBalance.spec.tsx
@@ -2,7 +2,9 @@ import { act, renderHook } from '@testing-library/react-hooks';
 import useSubscribedBalance from '../useSubscribedBalance';
 import Observable from '../../utils/observable';
 
-const mockData: Parameters<ReturnType<typeof useSubscribedBalance>['setBalanceData']>[0] = {
+type TBalance = Parameters<ReturnType<typeof useSubscribedBalance>['setBalanceData']>[0];
+
+const mockData: TBalance = {
     CR1: {
         balance: 10,
         converted_amount: 10,
@@ -18,10 +20,34 @@ describe('useSubscribedBalance', () => {
         const { result } = renderHook(() => useSubscribedBalance());
         const { setBalanceData } = result.current;
 
+        expect(result.current.isLoading).toEqual(true);
         expect(result.current.data).toBeUndefined();
         act(() => setBalanceData(mockData));
+        expect(result.current.isLoading).toBe(false);
         expect(result.current.data).toEqual(mockData);
     });
+    it('updates balance data when new data is set', () => {
+        const { result } = renderHook(() => useSubscribedBalance());
+        const { setBalanceData } = result.current;
+
+        act(() => setBalanceData(mockData));
+        expect(result.current.data).toEqual(mockData);
+
+        const updatedData: TBalance = {
+            CR1: {
+                balance: 20,
+                converted_amount: 20,
+                currency: 'USD',
+                demo_account: 0,
+                status: 1,
+                type: 'deriv',
+            },
+        };
+
+        act(() => setBalanceData(updatedData));
+        expect(result.current.data).toEqual(updatedData);
+    });
+
     it('does not set the data if the received data is the same as the existing data', () => {
         const { result } = renderHook(() => useSubscribedBalance());
         const { setBalanceData } = result.current;
